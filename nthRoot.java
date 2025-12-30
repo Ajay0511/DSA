1f6d530225c1cc2a4cd5626b8affd0428327118e
Ajay0511 <56833904+Ajay0511@users.noreply.github.com>
2025-12-30 10:38:56 +0530

    Create nthRoot.java

---
+++ nthRoot.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int nthRoot(int n, int m) {
+        // code here
+        int low = 0;
+        int high = m;
+        
+        while(low<=high){
+            int mid = (low+high)/2;
+            int val = (int) Math.pow(mid, n);
+            if(val == m){
+                return mid;
+            } else if(val>m){
+                high = mid-1;
+            } else{
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+}
