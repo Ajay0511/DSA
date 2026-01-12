51f25464f14f8eb95750ea4f4f9e8e98ddf938d9
Ajay0511 <56833904+Ajay0511@users.noreply.github.com>
2026-01-12 20:58:30 +0530

    https://leetcode.com/problems/minimum-time-visiting-all-points

--- Arrays/maximal_rectangle.py
+++ Arrays/maximal_rectangle.py
@@ -24,4 +24,34 @@ Constraints:
 rows == matrix.length
 cols == matrix[i].length
 1 <= rows, cols <= 200
-matrix[i][j] is '0' or '1'."""
+matrix[i][j] is '0' or '1'.
+"""
+
+from typing import List
+
+class Solution:
+
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        def largestHistogramRectangleArea(heights: List[int]) -> int:
+            heights.append(0)
+            maxArea = 0
+            st = []   
+            for i, curr_height in enumerate(heights):
+                while st and heights[st[-1]] > curr_height:
+                    idx = st.pop()                  
+                    prev_height = heights[idx]      
+                    width = i - st[-1] - 1 if st else i
+                    maxArea = max(prev_height * width, maxArea)
+                st.append(i)
+            return maxArea
+        
+        maxArea = 0
+        heights = [0] * (len(matrix[0]) + 1)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if(matrix[i][j] == '1'):
+                    heights[j] += int(matrix[i][j])
+                else:
+                    heights[j] = 0
+            maxArea = max(maxArea, largestHistogramRectangleArea(heights))
+        return maxArea
