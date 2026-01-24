31e67f418cafb9c2602ef30c2090605bdeb2a3bb
Ajay0511 <56833904+Ajay0511@users.noreply.github.com>
2026-01-24 19:30:28 +0530

    https://leetcode.com/problems/find-median-from-data-stream/description/
    Implement MedianFinder class to find the median of a data stream.

---
+++ Heap/median_data_stream.java
@@ -0,0 +1,61 @@
+/*
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+ 
+
+Example 1:
+
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+
+*/
+import java.util.*;
+
+class MedianFinder {
+
+    private PriorityQueue<Integer> small; // max-heap
+    private PriorityQueue<Integer> large; // min-heap
+
+    public MedianFinder() {
+        small = new PriorityQueue<>(Collections.reverseOrder());
+        large = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        // Step 1: add to max-heap
+        small.add(num);
+
+        // Step 2: balance ordering
+        large.add(small.poll());
+
+        // Step 3: balance sizes
+        if (large.size() > small.size()) {
+            small.add(large.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (small.size() > large.size()) {
+            return small.peek();
+        }
+        return (small.peek() + large.peek()) / 2.0;
+    }
+}
