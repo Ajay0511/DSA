5043075b478158998ba62070653669dfc0021ef2
Ajay0511 <56833904+Ajay0511@users.noreply.github.com>
2026-03-31 21:34:01 +0530

    Add countAndSay function for generating sequence
    Implement the count-and-say sequence function with examples and constraints.

---
+++ Strings/CountAndSay.py
@@ -0,0 +1,47 @@
+"""
+The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+countAndSay(1) = "1"
+countAndSay(n) is the run-length encoding of countAndSay(n - 1).
+Run-length encoding (RLE) is a string compression method that works by replacing consecutive identical characters (repeated 2 or more times) with the concatenation of the character and the number marking the count of the characters (length of the run). For example, to compress the string "3322251" we replace "33" with "23", replace "222" with "32", replace "5" with "15" and replace "1" with "11". Thus the compressed string becomes "23321511".
+
+Given a positive integer n, return the nth element of the count-and-say sequence.
+
+Example 1:
+Input: n = 4
+Output: "1211"
+Explanation:
+countAndSay(1) = "1"
+countAndSay(2) = RLE of "1" = "11"
+countAndSay(3) = RLE of "11" = "21"
+countAndSay(4) = RLE of "21" = "1211"
+
+Example 2:
+Input: n = 1
+Output: "1"
+Explanation:
+This is the base case.
+
+
+Constraints:
+1 <= n <= 30
+Follow up: Could you solve it iteratively?
+"""
+
+def countAndSay(self, n: int) -> str:
+        new_s = "1"
+        for i in range(n-1):
+            s = new_s[0]
+            temp = ''
+            c = 0
+            for l in new_s:
+                if l == s:
+                    c+=1
+                else:
+                    temp += str(c) + s
+                    c = 1
+                    s = l
+            temp += str(c) + s
+            new_s = temp
+
+        return new_s
