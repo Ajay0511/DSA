6b329ccec116eb49b0ce20b162879bbe057ae4db
Ajay0511 <56833904+Ajay0511@users.noreply.github.com>
2026-05-06 23:51:11 +0530

    Implement zigzag level order traversal for binary tree

--- Trees/LevelOrderSpiral.py
+++ Trees/LevelOrderSpiral.py
@@ -23,14 +23,45 @@ Constraints:
 The number of nodes in the tree is in the range [0, 2000].
 -1000 <= Node.val <= 1000
 """
-def helper(node, level):
-            if not node:
-                return
-            res[level].append(node.val)
-            helper(node.left, level + 1)
-            helper(node.right, level + 1)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return 
+        stack1 = [root]
+        stack2 = []
+        ans = []
 
-        helper(root, 0)
+        while stack1 or stack2:
+            curr = []
 
-        # Convert to a list, reversing every alternate level for zigzag order
-        return [vals if i % 2 == 0 else vals[::-1] for i, vals in sorted(res.items())]
+            while(stack1):
+                node = stack1.pop()
+                if node.left:
+                    stack2.append(node.left)
+                if node.right:
+                    stack2.append(node.right)
+                curr.append(node.val)
+            
+            if curr:
+                ans.append(curr)
+                curr = []
+            
+
+            while(stack2):
+                node = stack2.pop()
+                if node.right:
+                    stack1.append(node.right)
+                if node.left:
+                    stack1.append(node.left)
+                curr.append(node.val)
+            if curr:
+                ans.append(curr)
+                curr = []
+
+        return ans
