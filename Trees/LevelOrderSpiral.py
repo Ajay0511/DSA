346da4ee5f1eac61c6dab684513ba1555d50fa85
Ajay0511 <56833904+Ajay0511@users.noreply.github.com>
2026-05-06 23:48:02 +0530

    Add levelOrder function for binary tree traversal
    Implement level order traversal for binary tree.

---
+++ Trees/LevelOrderSpiral.py
@@ -0,0 +1,49 @@
+"""
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000
+"""
+
+def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
+        result = []
+        queue = deque([root])
+        
+        while queue:
+            level_size = len(queue)
+            level_nodes = []
+            
+            for _ in range(level_size):
+                node = queue.popleft()  # Remove front node
+                level_nodes.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)  # Add left child to queue
+                if node.right:
+                    queue.append(node.right)  # Add right child to queue
+            
+            result.append(level_nodes)
+        
+        return result
