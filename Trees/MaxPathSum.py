571e365754eea0e479a0c040b7881b4a74c6f11f
Ajay0511 <56833904+Ajay0511@users.noreply.github.com>
2026-05-31 15:22:46 +0530

    Create MaxPathSum.py

---
+++ Trees/MaxPathSum.py
@@ -0,0 +1,33 @@
+"""
+A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+The path sum of a path is the sum of the node's values in the path.
+Given the root of a binary tree, return the maximum path sum of any non-empty path.
+
+ 
+Example 1:
+Input: root = [1,2,3]
+Output: 6
+Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
+
+Example 2:
+Input: root = [-10,9,20,null,null,15,7]
+Output: 42
+Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
+"""
+
+def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        import sys
+        self.res = -sys.maxsize
+        if not root:
+            return 0
+        def helper(root):
+            if not root:
+                return 0
+            leftSum = helper(root.left)
+            rightSum = helper(root.right)
+
+            self.res = max(self.res, root.val+leftSum+rightSum)         
+
+            return max(root.val + max(leftSum,rightSum),0)          #done because only one path can be taken 
+        helper(root)
+        return self.res
